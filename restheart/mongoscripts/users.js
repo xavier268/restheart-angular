974077a311151fc184a19eae98633a0a89f050f5
Xavier Gandillot (from wor:wqkstation fed24.dellXPS) <xavier@gandillot.com>
2016-10-20 13:43:23 +0200

    scripts

--- restheart/mongoscripts/users.js
+++ restheart/mongoscripts/users.js
@@ -1,7 +1,10 @@
 // Script to load the database with the admin and first users
 
 print("Preparing to init authentication records in auth/users , if not done already");
-var conn = new Mongo("mongo");
+//                ############
+var conn = new Mongo("mongo"); // <= Connection string !! Caution !!
+//                ############
+
 var db = conn.getDB("auth");
 try {
     db.users.insertOne({_id: "admin", password: "temp", roles: ['admins', 'users']});
