c63644d14bd36f536403b230c5ca95983cab4167
Xavier Gandillot (from wor:wqkstation fed24.dellXPS) <xavier@gandillot.com>
2016-10-21 18:56:30 +0200

    (bugs) scripts

--- makeAll.sh
+++ makeAll.sh
@@ -1,23 +1,23 @@
 #! /bin/bash
 
-echo "Installing and building everything"
+echo "Installing and building everything then reconstructing the docker images"
 
 ## Mongo
 cd mongo
-docker build .
+docker build -t mongo .
 cd ..
 
 ## Restheart
 cd restheart 
 mvn clean install 
-docker build .
+docker build -t restheart .
 cd ..
 
 ## Client-main
 cd client
 npm install
 npm run aot
-docker build .
+docker build -t client .
 cd ..
 
 echo "Done"
--- pushDockerImages.sh
+++ pushDockerImages.sh
@@ -2,11 +2,13 @@
 
 MYREP="057259519792.dkr.ecr.eu-west-1.amazonaws.com"
 WHEN=$(date -u +%Y-%m-%d-%H-%m)
-
+echo "Preparing to push client, mongo and restheart images to $MYREPO"
 docker tag mongo:latest $MYREP/mongo:$WHEN
 docker tag restheart:latest $MYREP/restheart:$WHEN
-docker tag mongo:latest $MYREP/client:$WHEN
+docker tag client:latest $MYREP/client:$WHEN
 
-docker push $MYREP/mongo:$WHEN
-docker push $MYREP/restheart:$WHEN
-docker push $MYREP/client:$WHEN
+docker push $MYREP/mongo:$WHEN &
+docker push $MYREP/restheart:$WHEN &
+docker push $MYREP/client:$WHEN &
+
+echo "Pushing all the images tagged with :$WHEN initited in the background"
