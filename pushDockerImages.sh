327b65f82fe234d9bea6d2333c673d5ef094be6e
Xavier Gandillot (from wor:wqkstation fed24.dellXPS) <xavier@gandillot.com>
2016-10-22 00:07:07 +0200

    (script) corrected image labels

--- pushDockerImages.sh
+++ pushDockerImages.sh
@@ -1,14 +1,14 @@
 #! /bin/bash
 
 MYREP="057259519792.dkr.ecr.eu-west-1.amazonaws.com"
-WHEN=$(date -u +%Y-%m-%d-%H-%m)
+WHEN=$(date -u +%Y-%m-%d-%H-%M)
 echo "Preparing to push client, mongo and restheart images to $MYREPO"
 docker tag mongo:latest $MYREP/mongo:$WHEN
 docker tag restheart:latest $MYREP/restheart:$WHEN
 docker tag client:latest $MYREP/client:$WHEN
 
-docker push $MYREP/mongo:$WHEN 
-docker push $MYREP/restheart:$WHEN 
-docker push $MYREP/client:$WHEN 
+docker push $MYREP/mongo:$WHEN
+docker push $MYREP/restheart:$WHEN
+docker push $MYREP/client:$WHEN
 
-echo "All images tagged with :$WHEN were pushed"
+echo "All images tagged with :$WHEN have been pushed to $MYREP"
